0b52850c24ee029e99a00a50dfe5e4fe9df538db
Trisha Dwivedi <trisha.dwivedi.99@gmail.com>
2015-08-06 10:18:01 -0700

    commit 8/6 10:17 am

---
+++ TemplateProject/TealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TealTableViewCell.swift
+//  TemplateProject
+//
+//  Created by Trisha Dwivedi on 8/6/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class TealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TemplateProject/YellowGreenTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  YellowGreenTableViewCell.swift
+//  TemplateProject
+//
+//  Created by Trisha Dwivedi on 8/6/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class YellowGreenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
